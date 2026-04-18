eeb702ea27e894130715ad5f68db552371b0f302
Blnkhole <haianh0772241884@gmail.com>
2026-04-19 00:28:36 +0700

    Update Random2.py

--- Softmax Regression/Random2.py
+++ Softmax Regression/Random2.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 # Load your CSV
 df = pd.read_csv("creditcard.csv")
 
-train_df, test_df = train_test_split(df, test_size = 0.25, random_state = 42, stratify = df['Class']) #stratify: dam bao ty le class
+train_df, test_df = train_test_split(df, test_size = 0.25, random_state = 42, stratify = df['Class']) #stratify: ensure class ratio
 
 # Optional: Save to CSV
 train_df.to_csv('creditcard_train.csv', index=False)
